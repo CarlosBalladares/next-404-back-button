2dec017b23777cf0633e055efa5edf57b021c040
Carlos Balladares <carlosbf94@hotmail.com>
2023-08-07 14:06:10 -0400

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,31 @@
-/** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null
-}
+import Link from "next/link";
+import { notFound } from "next/navigation";
+const Page = (props: { searchParams: { test?: string } }) => {
+  // This would happen if you were searching for something in your db and it was not found
+  if (props.searchParams.test) {
+    notFound();
+  }
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "20px",
+        padding: "20px 0px 0px 0px",
+      }}
+    >
+      <h1>This page demonstrates the issue.</h1>
+      <p>
+        Expected: click on the link below, 404 appears, go back and this page
+        shows.
+      </p>
+      <p>Actual: link is clicked and 404 appears, go back and 404 remains </p>
+      <Link href={{ href: "/", query: { test: "true" } }}>
+        click to demonstrate
+      </Link>
+    </div>
+  );
+};
+
+export default Page;
